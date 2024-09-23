da99d8329904b47200f9778c28dfb2ac5ccc149e
Tomas Zelaschi <totozelaschi@gmail.com>
2024-09-23 18:38:33 -0300

    setup SecurityCameraControllerTest

---
+++ SmartHome.WebApi/Controllers/SecurityCameraController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartHome.BusinessLogic.Interfaces;
+
+namespace SmartHome.WebApi.Controllers;
+
+[Route("api/v1/securityCameras")]
+[ApiController]
+public class SecurityCameraController : ControllerBase
+{
+    private readonly ISecurityCameraLogic _securityCameraLogic;
+    public SecurityCameraController(ISecurityCameraLogic securityCameraLogic)
+    {
+        _securityCameraLogic = securityCameraLogic;
+    }
+}
---
+++ SmartHome.WebApiTest/SecurityCameraControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SmartHome.BusinessLogic.Domain;
+using SmartHome.BusinessLogic.Interfaces;
+using SmartHome.WebApi.Controllers;
+
+namespace SmartHome.WebApiTest;
+[TestClass]
+public class SecurityCameraControllerTest
+{
+    private Mock<ISecurityCameraLogic>? securityCameraLogicMock;
+    private SecurityCameraController? securityCameraController;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        securityCameraLogicMock = new Mock<ISecurityCameraLogic>(MockBehavior.Strict);
+        securityCameraController = new SecurityCameraController(securityCameraLogicMock.Object);
+    }
+}
