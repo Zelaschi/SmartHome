08e497542c8647a97f5c8e6066748b505b8e3b14
Zelaschi <totozelaschi@gmail.com>
2024-10-17 14:58:15 -0700

    new devices classes

---
+++ SmartHome.BusinessLogic/Domain/InteligentLamp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.BusinessLogic.Domain;
+public class InteligentLamp : Device
+{
+    public bool IsOn { get; set; }
+}
