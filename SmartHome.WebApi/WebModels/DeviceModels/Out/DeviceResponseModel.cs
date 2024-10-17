647043840d2f0d47c7193c16e9cd4a984d35e324
Zelaschi <totozelaschi@gmail.com>
2024-10-17 15:49:58 -0700

    Restored a file that I deleted on previous commit but was also used somewhere else

---
+++ SmartHome.WebApi/WebModels/DeviceModels/Out/DeviceResponseModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHome.BusinessLogic.Domain;
+
+namespace SmartHome.WebApi.WebModels.DeviceModels.Out;
+public sealed class DeviceResponseModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string ModelNumber { get; set; }
+    public string Description { get; set; }
+    public string Photos { get; set; }
+    public string CompanyName { get; set; }
+
+    public DeviceResponseModel(Device device)
+    {
+        Id = device.Id;
+        Name = device.Name;
+        ModelNumber = device.ModelNumber;
+        Description = device.Description;
+        Photos = device.Photos;
+        CompanyName = device.Business.Name;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is DeviceResponseModel d && d.Id == Id;
+    }
+}
