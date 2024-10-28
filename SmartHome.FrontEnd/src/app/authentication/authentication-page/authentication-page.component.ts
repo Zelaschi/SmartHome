7cc1f691759c6855539ff1742921b944a11db134
Pedro Azambuja <pedroaza02@hotmail.com>
2024-10-28 16:38:47 -0300

    room migration and changes in frontend wrong branch

--- SmartHome.FrontEnd/src/app/authentication/authentication-page/authentication-page.component.ts
+++ SmartHome.FrontEnd/src/app/authentication/authentication-page/authentication-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-authentication-page',
   templateUrl: './authentication-page.component.html',
-  styleUrls: ['./authentication-page.component.css']
+  styles: ``
 })
-export class AuthenticationPageComponent{
+export class AuthenticationPageComponent {
+
 }
--- SmartHome.FrontEnd/src/app/authentication/authentication-routing.module.ts
+++ SmartHome.FrontEnd/src/app/authentication/authentication-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { AuthenticationPageComponent } from './authentication-page/authentication-page.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthenticationPageComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginFormComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterFormComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthenticationRoutingModule { }
+export class AuthenticationRoutingModule {}
