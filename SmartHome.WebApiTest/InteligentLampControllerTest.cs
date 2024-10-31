6b61f92cf93edc53e74a52f27246b17f1aa7a504
mfel3004 <martinfelder19@gmail.com>
2024-10-31 11:31:05 -0300

    [GREEN] InteligentLampsController_NullCreateDeviceLogic_ThrowsArgumentNullException and CreateInteligentLampTest_UserIdMissing

--- SmartHome.WebApiTest/InteligentLampControllerTest.cs
+++ SmartHome.WebApiTest/InteligentLampControllerTest.cs
@@ -31,10 +31,8 @@ public class InteligentLampControllerTest
     [TestMethod]
     public void CreateInteligentLampTest_Ok()
     {
-        // Arrange
         var user1 = new User() { Id = Guid.NewGuid(), Name = "a", Surname = "b", Password = "psw1", Email = "mail1@mail.com", Role = homeOwner, CreationDate = DateTime.Today };
         var company1 = new Business() { Id = Guid.NewGuid(), Name = "hikvision", Logo = "logo1", RUT = "rut1", BusinessOwner = user1 };
-
         var deviceRequestModel = new InteligentLampRequestModel()
         {
             Name = "Lampara inteligente",
@@ -46,28 +44,69 @@ public class InteligentLampControllerTest
         Device device = deviceRequestModel.ToEntity();
         device.Business = company1;
         device.Id = Guid.NewGuid();
-
         var httpContext = new DefaultHttpContext();
         httpContext.Items.Add("User", user1);
-
         var controllerContext = new ControllerContext()
         {
             HttpContext = httpContext
         };
 
         inteligentLampsController = new InteligentLampsController(inteligentLampLogicMock.Object) { ControllerContext = controllerContext };
-
         inteligentLampLogicMock.Setup(d => d.CreateDevice(It.IsAny<Device>(), It.IsAny<User>(), It.IsAny<string>())).Returns(device);
-
         var expectedResult = new InteligentLampResponseModel(device);
         var expectedObjectResult = new CreatedAtActionResult("CreateInteligentLamp", "InteligentLamp", new { Id = device.Id }, expectedResult);
 
-        // Act
         var result = inteligentLampsController.CreateInteligentLamp(deviceRequestModel) as CreatedAtActionResult;
         var deviceResult = result.Value as InteligentLampResponseModel;
 
-        // Assert
         inteligentLampLogicMock.VerifyAll();
         Assert.IsTrue(expectedObjectResult.StatusCode.Equals(result.StatusCode) && expectedResult.Equals(deviceResult));
     }
+
+    [TestMethod]
+    public void CreateInteligentLampTest_UserIdMissing()
+    {
+        // Arrange
+        var deviceRequestModel = new InteligentLampRequestModel()
+        {
+            Name = "Lampara inteligente",
+            Description = "Lampara inteligente",
+            ModelNumber = "1234",
+            Photos = []
+        };
+
+        // Configuramos el contexto de la solicitud sin el usuario
+        var httpContext = new DefaultHttpContext();
+        var controllerContext = new ControllerContext()
+        {
+            HttpContext = httpContext
+        };
+
+        inteligentLampsController = new InteligentLampsController(inteligentLampLogicMock.Object)
+        {
+            ControllerContext = controllerContext
+        };
+
+        // Act
+        var result = inteligentLampsController.CreateInteligentLamp(deviceRequestModel) as UnauthorizedObjectResult;
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(401, result.StatusCode);
+        Assert.AreEqual("UserId is missing", result.Value);
+    }
+
+    [TestMethod]
+    public void InteligentLampsController_NullCreateDeviceLogic_ThrowsArgumentNullException()
+    {
+        try
+        {
+            var controller = new InteligentLampsController(null);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Assert.AreEqual("createDeviceLogic", ex.ParamName);
+        }
+    }
+
 }
