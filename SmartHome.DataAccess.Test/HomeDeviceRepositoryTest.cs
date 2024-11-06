00b6d5fb43ee25b46c0f2be9913076b1cb90de62
mfel3004 <martinfelder19@gmail.com>
2024-11-05 21:08:02 -0300

    Setup HomeDeviceRepositoryTest

---
+++ SmartHome.DataAccess.Test/HomeDeviceRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using SmartHome.BusinessLogic.Domain;
+using SmartHome.DataAccess.Contexts;
+using SmartHome.DataAccess.CustomExceptions;
+using SmartHome.DataAccess.Repositories;
+
+namespace SmartHome.DataAccess.Test;
+public class HomeDeviceRepositoryTest
+{
+    private readonly SmartHomeEFCoreContext _context;
+    private readonly HomeDeviceRepository _homeDeviceRepository;
+
+    public HomeDeviceRepositoryTest()
+    {
+        _context = DbContextBuilder.BuildTestDbContext();
+        _homeDeviceRepository = new HomeDeviceRepository(_context);
+    }
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _context.Database.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context.Database.EnsureDeleted();
+    }
+}
