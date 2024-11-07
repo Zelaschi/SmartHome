ab6017352e19f788f7fdb15acb8d4e18aeb7dcd8
mfel3004 <martinfelder19@gmail.com>
2024-11-07 18:47:01 -0300

    [GREEN] Update_WhenHomeMemberDoesNotExist_ShouldThrowDatabaseException

--- SmartHome.DataAccess.Test/HomeMemberRepositoryTest.cs
+++ SmartHome.DataAccess.Test/HomeMemberRepositoryTest.cs
@@ -282,6 +282,161 @@ public class HomeMemberRepositoryTest
     #endregion
 
     #region Update
+    [TestMethod]
+    public void Update_WhenHomeMemberExists_ShouldUpdateInDataBase()
+    {
+        var role = new Role
+        {
+            Id = Guid.NewGuid(),
+            Name = "User Role"
+        };
+        _context.Roles.Add(role);
+        _context.SaveChanges();
+
+        var owner = new User
+        {
+            Name = "Owner Name",
+            Surname = "Owner Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(owner);
+        _context.SaveChanges();
+
+        var user = new User
+        {
+            Name = "User Name",
+            Surname = "User Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        var home = new Home
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Home",
+            MainStreet = "Test Street",
+            DoorNumber = "123",
+            Latitude = "0.0000",
+            Longitude = "0.0000",
+            MaxMembers = 4,
+            Owner = owner
+        };
+        _context.Homes.Add(home);
+        _context.SaveChanges();
+
+        var homeMember = new HomeMember(user)
+        {
+            HomeId = home.Id
+        };
+        _homeMemberRepository.Add(homeMember);
+        _context.SaveChanges();
+
+        user.Name = "Updated User Name";
+        user.Surname = "Updated User Surname";
+        user.Password = "Updated TestPassword123";
+        user.Email = "Updated test@example.com";
+        _context.Users.Update(user);
+        _context.SaveChanges();
+
+        var updatedHomeMember = new HomeMember
+        {
+            HomeMemberId = homeMember.HomeMemberId,
+            HomeId = home.Id,
+            User = user
+        };
+
+        var result = _homeMemberRepository.Update(updatedHomeMember);
+
+        result.Should().NotBeNull();
+        result.HomeMemberId.Should().Be(updatedHomeMember.HomeMemberId);
+        result.User.Name.Should().Be("Updated User Name");
+
+        var updatedEntityInDb = _context.HomeMembers
+            .Include(hm => hm.User)
+            .FirstOrDefault(hm => hm.HomeMemberId == homeMember.HomeMemberId);
+
+        updatedEntityInDb.Should().NotBeNull();
+        updatedEntityInDb.User.Name.Should().Be("Updated User Name");
+    }
+
+    [TestMethod]
+    public void Update_WhenHomeMemberDoesNotExist_ShouldThrowDatabaseException()
+    {
+        var role = new Role
+        {
+            Id = Guid.NewGuid(),
+            Name = "User Role"
+        };
+        _context.Roles.Add(role);
+        _context.SaveChanges();
+
+        var owner = new User
+        {
+            Name = "Owner Name",
+            Surname = "Owner Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(owner);
+        _context.SaveChanges();
+
+        var user = new User
+        {
+            Name = "User Name",
+            Surname = "User Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        var home = new Home
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Home",
+            MainStreet = "Test Street",
+            DoorNumber = "123",
+            Latitude = "0.0000",
+            Longitude = "0.0000",
+            MaxMembers = 4,
+            Owner = owner
+        };
+        _context.Homes.Add(home);
+        _context.SaveChanges();
+
+        var homeMember = new HomeMember(user)
+        {
+            HomeId = home.Id
+        };
+        _homeMemberRepository.Add(homeMember);
+        _context.SaveChanges();
+
+        user.Name = "Updated User Name";
+        user.Surname = "Updated User Surname";
+        user.Password = "Updated TestPassword123";
+        user.Email = "Updated test@example.com";
+        _context.Users.Update(user);
+        _context.SaveChanges();
+
+        var nonExistingHomeMember= new HomeMember
+        {
+            HomeMemberId = Guid.NewGuid(),
+            HomeId = home.Id,
+            User = user
+        };
+
+        Action action = () => _homeMemberRepository.Update(nonExistingHomeMember);
+
+        action.Should().Throw<DatabaseException>()
+            .WithMessage("The HomeMember does not exist in the Data Base.");
+    }
     #endregion
 
     #region FindAll
