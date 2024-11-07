28c0a51a18a8232a41b4bfcb2aa40259fb6f916e
mfel3004 <martinfelder19@gmail.com>
2024-11-06 21:57:45 -0300

    [GREEN] Delete_WhenHomeDeviceExists_ShouldRemoveFromDatabase and Delete_WhenHomeDeviceDoesNotExists_ShouldThrowException

--- SmartHome.DataAccess.Test/HomeDeviceRepositoryTest.cs
+++ SmartHome.DataAccess.Test/HomeDeviceRepositoryTest.cs
@@ -173,4 +173,135 @@ public class HomeDeviceRepositoryTest
         homeDevices[0].Name.Should().Be(homeDevice.Name);
     }
     #endregion
+
+    #region Delete
+    [TestMethod]
+    public void Delete_WhenHomeDeviceExists_ShouldRemoveFromDatabase()
+    {
+        var device = new Device
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Device",
+            ModelNumber = "Model123",
+            Description = "Test Description",
+            Photos = new List<Photo>()
+        };
+
+        _context.Devices.Add(device);
+        _context.SaveChanges();
+
+        var role = new Role
+        {
+            Id = Guid.NewGuid(),
+            Name = "User Role"
+        };
+        _context.Roles.Add(role);
+        _context.SaveChanges();
+
+        var owner = new User
+        {
+            Name = "Test Name",
+            Surname = "Test Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+
+        _context.Users.Add(owner);
+        _context.SaveChanges();
+
+        var home = new Home
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Home",
+            MainStreet = "Test Street",
+            DoorNumber = "123",
+            Latitude = "0.0000",
+            Longitude = "0.0000",
+            MaxMembers = 4,
+            Owner = owner
+        };
+
+        _context.Homes.Add(home);
+        _context.SaveChanges();
+
+        var homeDevice = new HomeDevice
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test HomeDevice",
+            Device = device,
+            HomeId = home.Id,
+            Online = true
+        };
+        _homeDeviceRepository.Add(homeDevice);
+        _context.SaveChanges();
+
+        _homeDeviceRepository.Delete(homeDevice.Id);
+
+        _context.HomeDevices.FirstOrDefault(b => b.Id == homeDevice.Id).Should().BeNull();
+    }
+
+    [TestMethod]
+    public void Delete_WhenHomeDeviceDoesNotExists_ShouldThrowException()
+    {
+        var device = new Device
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Device",
+            ModelNumber = "Model123",
+            Description = "Test Description",
+            Photos = new List<Photo>()
+        };
+
+        _context.Devices.Add(device);
+        _context.SaveChanges();
+
+        var role = new Role
+        {
+            Id = Guid.NewGuid(),
+            Name = "User Role"
+        };
+        _context.Roles.Add(role);
+        _context.SaveChanges();
+
+        var owner = new User
+        {
+            Name = "Test Name",
+            Surname = "Test Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(owner);
+        _context.SaveChanges();
+
+        var home = new Home
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Home",
+            MainStreet = "Test Street",
+            DoorNumber = "123",
+            Latitude = "0.0000",
+            Longitude = "0.0000",
+            MaxMembers = 4,
+            Owner = owner
+        };
+        _context.Homes.Add(home);
+        _context.SaveChanges();
+
+        var homeDevice = new HomeDevice
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test HomeDevice",
+            Device = device,
+            HomeId = home.Id,
+            Online = true
+        };
+
+        Action action = () => _homeDeviceRepository.Delete(homeDevice.Id);
+
+        action.Should().Throw<DatabaseException>()
+            .WithMessage("The Home Device does not exist in the Data Base.");
+    }
+    #endregion
 }
