7e4f83e94910862e83e5e49ce9704e09470bc16f
mfel3004 <martinfelder19@gmail.com>
2024-11-07 18:22:28 -0300

    [GREEN] FindAll_WhenExistOnlyOne_ShouldReturnOneHomeMember

--- SmartHome.DataAccess.Test/HomeMemberRepositoryTest.cs
+++ SmartHome.DataAccess.Test/HomeMemberRepositoryTest.cs
@@ -219,12 +219,131 @@ public class HomeMemberRepositoryTest
     #endregion
 
     #region Find
+    [TestMethod]
+    public void Find_WhenHomeMemberExists_ShouldReturnHomeMember()
+    {
+        var role = new Role
+        {
+            Id = Guid.NewGuid(),
+            Name = "User Role"
+        };
+        _context.Roles.Add(role);
+        _context.SaveChanges();
+
+        var owner = new User
+        {
+            Name = "Owner Name",
+            Surname = "Owner Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(owner);
+        _context.SaveChanges();
+
+        var user = new User
+        {
+            Name = "User Name",
+            Surname = "User Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        var home = new Home
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Home",
+            MainStreet = "Test Street",
+            DoorNumber = "123",
+            Latitude = "0.0000",
+            Longitude = "0.0000",
+            MaxMembers = 4,
+            Owner = owner
+        };
+        _context.Homes.Add(home);
+        _context.SaveChanges();
+
+        var homeMember = new HomeMember(user)
+        {
+            HomeId = home.Id
+        };
+        _homeMemberRepository.Add(homeMember);
+        _context.SaveChanges();
+
+        var result = _homeMemberRepository.Find(hm => hm.HomeMemberId == homeMember.HomeMemberId);
+
+        result.Should().NotBeNull();
+        result.HomeMemberId.Should().Be(homeMember.HomeMemberId);
+        result.User.Name.Should().Be(homeMember.User.Name);
+    }
     #endregion
 
     #region Update
     #endregion
 
-    #region GetAll
-    #endregion
+    #region FindAll
+    [TestMethod]
+    public void FindAll_WhenExistOnlyOne_ShouldReturnOneHomeMember()
+    {
+        var role = new Role
+        {
+            Id = Guid.NewGuid(),
+            Name = "User Role"
+        };
+        _context.Roles.Add(role);
+        _context.SaveChanges();
 
+        var owner = new User
+        {
+            Name = "Owner Name",
+            Surname = "Owner Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(owner);
+        _context.SaveChanges();
+
+        var user = new User
+        {
+            Name = "User Name",
+            Surname = "User Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        var home = new Home
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Home",
+            MainStreet = "Test Street",
+            DoorNumber = "123",
+            Latitude = "0.0000",
+            Longitude = "0.0000",
+            MaxMembers = 4,
+            Owner = owner
+        };
+        _context.Homes.Add(home);
+        _context.SaveChanges();
+
+        var homeMember = new HomeMember(user)
+        {
+            HomeId = home.Id
+        };
+        _homeMemberRepository.Add(homeMember);
+        _context.SaveChanges();
+
+        var homeMembers = _homeMemberRepository.FindAll();
+
+        homeMembers.Count.Should().Be(1);
+        homeMembers[0].HomeMemberId.Should().Be(homeMember.HomeMemberId);
+        homeMembers[0].User.Name.Should().Be(homeMember.User.Name);
+    }
+    #endregion
 }
