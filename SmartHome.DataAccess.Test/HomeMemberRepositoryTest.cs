a3a6d754d13674b5836e657b2ae0b4d20330d93b
mfel3004 <martinfelder19@gmail.com>
2024-11-07 13:10:05 -0300

    Build

---
+++ SmartHome.DataAccess.Test/HomeMemberRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using SmartHome.BusinessLogic.Domain;
+using SmartHome.DataAccess.Contexts;
+using SmartHome.DataAccess.CustomExceptions;
+using SmartHome.DataAccess.Repositories;
+
+namespace SmartHome.DataAccess.Test;
+[TestClass]
+public class HomeMemberRepositoryTest
+{
+    private readonly SmartHomeEFCoreContext _context;
+    private readonly HomeMemberRepository _homeMemberRepository;
+
+    public HomeMemberRepositoryTest()
+    {
+        _context = DbContextBuilder.BuildTestDbContext();
+        _homeMemberRepository = new HomeMemberRepository(_context);
+    }
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _context.Database.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context.Database.EnsureDeleted();
+    }
+}
