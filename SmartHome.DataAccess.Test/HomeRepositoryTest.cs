44898cc165db2a3406773808f235f0621d5aeac1
mfel3004 <martinfelder19@gmail.com>
2024-11-07 20:33:29 -0300

    [GREEN] Add_HomeEntity_ShouldReturnHomeEntity

--- SmartHome.DataAccess.Test/HomeRepositoryTest.cs
+++ SmartHome.DataAccess.Test/HomeRepositoryTest.cs
@@ -30,4 +30,63 @@ public class HomeRepositoryTest
     {
         _context.Database.EnsureDeleted();
     }
+
+    #region Add
+    [TestMethod]
+    public void Add_HomeEntity_ShouldReturnHomeEntity()
+    {
+        var role = new Role
+        {
+            Id = Guid.NewGuid(),
+            Name = "User Role"
+        };
+        _context.Roles.Add(role);
+        _context.SaveChanges();
+
+        var owner = new User
+        {
+            Name = "Test Name",
+            Surname = "Test Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(owner);
+        _context.SaveChanges();
+
+        var home = new Home
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Home",
+            MainStreet = "Test Street",
+            DoorNumber = "123",
+            Latitude = "0.0000",
+            Longitude = "0.0000",
+            MaxMembers = 4,
+            Owner = owner
+        };
+        _context.Homes.Add(home);
+        _context.SaveChanges();
+
+        using var otherContext = DbContextBuilder.BuildTestDbContext();
+        var homesSaved = otherContext.Homes.ToList();
+
+        homesSaved.Count.Should().Be(1);
+        var homeSaved = homesSaved[0];
+        homeSaved.Id.Should().Be(home.Id);
+        homeSaved.Name.Should().Be(home.Name);
+    }
+    #endregion
+
+    #region Delete
+    #endregion
+
+    #region Update
+    #endregion
+
+    #region Find
+    #endregion
+
+    #region GetAll
+    #endregion
 }
