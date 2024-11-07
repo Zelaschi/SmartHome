99ab4a4719cad03d46998c6b44655a2bb0885612
mfel3004 <martinfelder19@gmail.com>
2024-11-07 15:43:49 -0300

    [GREEN] Delete_WhenHomeMemberDoesNotExists_ShouldThrowException

--- SmartHome.DataAccess.Test/HomeMemberRepositoryTest.cs
+++ SmartHome.DataAccess.Test/HomeMemberRepositoryTest.cs
@@ -158,6 +158,64 @@ public class HomeMemberRepositoryTest
         _context.HomeMembers.FirstOrDefault(hm => hm.HomeMemberId == homeMember.HomeMemberId).Should().BeNull();
     }
 
+    [TestMethod]
+    public void Delete_WhenHomeMemberDoesNotExists_ShouldThrowException()
+    {
+        var role = new Role
+        {
+            Id = Guid.NewGuid(),
+            Name = "User Role"
+        };
+        _context.Roles.Add(role);
+        _context.SaveChanges();
+
+        var owner = new User
+        {
+            Name = "Owner Name",
+            Surname = "Owner Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(owner);
+        _context.SaveChanges();
+
+        var user = new User
+        {
+            Name = "User Name",
+            Surname = "User Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        var home = new Home
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Home",
+            MainStreet = "Test Street",
+            DoorNumber = "123",
+            Latitude = "0.0000",
+            Longitude = "0.0000",
+            MaxMembers = 4,
+            Owner = owner
+        };
+        _context.Homes.Add(home);
+        _context.SaveChanges();
+
+        var homeMember = new HomeMember(user)
+        {
+            HomeId = home.Id
+        };
+
+        Action action = () => _homeMemberRepository.Delete(homeMember.HomeMemberId);
+
+        action.Should().Throw<DatabaseException>()
+            .WithMessage("The HomeMember does not exist in the Data Base.");
+    }
+
     #endregion
 
     #region Find
