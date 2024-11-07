17e9398edd4c5e528296a2671ce4709e04a8f64c
mfel3004 <martinfelder19@gmail.com>
2024-11-07 18:55:13 -0300

    Build

---
+++ SmartHome.DataAccess.Test/HomePermissionRepositoryTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using SmartHome.BusinessLogic.Domain;
+using SmartHome.BusinessLogic.Services;
+using SmartHome.DataAccess.Contexts;
+using SmartHome.DataAccess.CustomExceptions;
+using SmartHome.DataAccess.Repositories;
+
+namespace SmartHome.DataAccess.Test;
+[TestClass]
+public class HomePermissionRepositoryTest
+{
+    private readonly SmartHomeEFCoreContext _context;
+    private readonly HomePermissionRepository _homePermissionRepository;
+
+    public HomePermissionRepositoryTest()
+    {
+        _context = DbContextBuilder.BuildTestDbContext();
+        _homePermissionRepository = new HomePermissionRepository(_context);
+    }
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _context.Database.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context.Database.EnsureDeleted();
+    }
+
+    #region Add
+    #endregion
+
+    #region Delete
+    #endregion
+
+    #region Find
+    #endregion
+
+    #region Update
+    #endregion
+
+    #region FindAll
+    #endregion
+}
