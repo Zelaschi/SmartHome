1dc4801b1ffe3dbf683b37a96784a800ad5233dc
mfel3004 <martinfelder19@gmail.com>
2024-11-07 21:19:42 -0300

    Build

---
+++ SmartHome.DataAccess.Test/RoleRepositroyTest.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using SmartHome.BusinessLogic.Domain;
+using SmartHome.DataAccess.Contexts;
+using SmartHome.DataAccess.CustomExceptions;
+using SmartHome.DataAccess.Repositories;
+
+namespace SmartHome.DataAccess.Test;
+
+[TestClass]
+public class RoleRepositoryTest
+{
+    private readonly SmartHomeEFCoreContext _context;
+    private readonly RoleRepository _roleRepository;
+
+    public RoleRepositoryTest()
+    {
+        _context = DbContextBuilder.BuildTestDbContext();
+        _roleRepository = new RoleRepository(_context);
+    }
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _context.Database.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context.Database.EnsureDeleted();
+    }
+
+    #region Add
+    #endregion
+
+    #region Delete
+    #endregion
+
+    #region Find
+    #endregion
+
+    #region Update
+    #endregion
+
+    #region GetAll
+    #endregion
+}
