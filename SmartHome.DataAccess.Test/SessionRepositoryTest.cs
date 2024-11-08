cf1948f2f62631a7feeba0d5997df4ea0ab57488
mfel3004 <martinfelder19@gmail.com>
2024-11-07 22:01:50 -0300

    Build

---
+++ SmartHome.DataAccess.Test/SessionRepositoryTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using SmartHome.BusinessLogic.Domain;
+using SmartHome.BusinessLogic.Services;
+using SmartHome.DataAccess.Contexts;
+using SmartHome.DataAccess.CustomExceptions;
+using SmartHome.DataAccess.Repositories;
+
+namespace SmartHome.DataAccess.Test;
+
+[TestClass]
+public class SessionRepositoryTest
+{
+    private readonly SmartHomeEFCoreContext _context;
+    private readonly SessionRepository _sessionRepository;
+
+    public SessionRepositoryTest()
+    {
+        _context = DbContextBuilder.BuildTestDbContext();
+        _sessionRepository = new SessionRepository(_context);
+    }
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _context.Database.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context.Database.EnsureDeleted();
+    }
+
+    #region Add
+    #endregion
+
+    #region Delete
+    #endregion
+
+    #region Update
+    #endregion
+
+    #region Find
+    #endregion
+
+    #region GetAll
+    #endregion
+}
