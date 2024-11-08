49d099beba088e403e4628d8ae193c514ad67a6f
mfel3004 <martinfelder19@gmail.com>
2024-11-07 21:41:53 -0300

    [GREEN] Add_WhenRoomIsAdded_ShouldReturnRoom

--- SmartHome.DataAccess.Test/RoomRepositoryTest.cs
+++ SmartHome.DataAccess.Test/RoomRepositoryTest.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using SmartHome.BusinessLogic.Domain;
+using SmartHome.BusinessLogic.Services;
 using SmartHome.DataAccess.Contexts;
 using SmartHome.DataAccess.CustomExceptions;
 using SmartHome.DataAccess.Repositories;
@@ -31,6 +32,58 @@ public class RoomRepositoryTest
     }
 
     #region Add
+    [TestMethod]
+    public void Add_WhenRoomIsAdded_ShouldReturnRoom()
+    {
+        var role = new Role
+        {
+            Id = Guid.NewGuid(),
+            Name = "User Role"
+        };
+        _context.Roles.Add(role);
+        _context.SaveChanges();
+
+        var owner = new User
+        {
+            Name = "Test Name",
+            Surname = "Test Surname",
+            Password = "TestPassword123",
+            Email = "test@example.com",
+            RoleId = role.Id
+        };
+        _context.Users.Add(owner);
+        _context.SaveChanges();
+
+        var home = new Home
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Home",
+            MainStreet = "Test Street",
+            DoorNumber = "123",
+            Latitude = "0.0000",
+            Longitude = "0.0000",
+            MaxMembers = 4,
+            Owner = owner
+        };
+        _context.Homes.Add(home);
+        _context.SaveChanges();
+
+        var room = new Room
+        {
+            Home = home,
+            Name = "Living Room",
+        };
+        _roomRepository.Add(room);
+        _context.SaveChanges();
+
+        using var otherContext = DbContextBuilder.BuildTestDbContext();
+        var roomsSaved = otherContext.Rooms.ToList();
+
+        roomsSaved.Count.Should().Be(1);
+        var roomSaved = roomsSaved[0];
+        roomSaved.Id.Should().Be(roomSaved.Id);
+        roomSaved.Name.Should().Be(roomSaved.Name);
+    }
     #endregion
 
     #region Update
