20c2a498ebb222f3c5ef315debc8d23137d35007
mfel3004 <martinfelder19@gmail.com>
2024-11-07 21:36:57 -0300

    Build

---
+++ SmartHome.DataAccess.Test/RoomRepositoryTest.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using SmartHome.BusinessLogic.Domain;
+using SmartHome.DataAccess.Contexts;
+using SmartHome.DataAccess.CustomExceptions;
+using SmartHome.DataAccess.Repositories;
+
+namespace SmartHome.DataAccess.Test;
+
+[TestClass]
+public class RoomRepositoryTest
+{
+    private readonly SmartHomeEFCoreContext _context;
+    private readonly RoomRepository _roomRepository;
+
+    public RoomRepositoryTest()
+    {
+        _context = DbContextBuilder.BuildTestDbContext();
+        _roomRepository = new RoomRepository(_context);
+    }
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _context.Database.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        _context.Database.EnsureDeleted();
+    }
+
+    #region Add
+    #endregion
+
+    #region Update
+    #endregion
+
+    #region Delete
+    #endregion
+
+    #region GetAll
+    #endregion
+
+    #region Find
+    #endregion
+}
