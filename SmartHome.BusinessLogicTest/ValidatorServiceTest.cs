0de7e068fb5493f2521452ed68a62f06e794e10c
mfel3004 <martinfelder19@gmail.com>
2024-11-13 18:48:14 -0300

    build ValidatorServiceTest

---
+++ SmartHome.BusinessLogicTest/ValidatorServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using SmartHome.BusinessLogic.Domain;
+using SmartHome.BusinessLogic.GenericRepositoryInterface;
+using SmartHome.BusinessLogic.Services;
+
+namespace SmartHome.BusinessLogic.Test;
+
+[TestClass]
+public class ValidatorServiceTest
+{
+    private Mock<IGenericRepository<ModelNumberValidator>>? validatorRepositoryMock;
+    private ValidatorService? validatorService;
+
+    [TestInitialize]
+
+    public void Initialize()
+    {
+        validatorRepositoryMock = new Mock<IGenericRepository<ModelNumberValidator>>();
+        validatorService = new ValidatorService(validatorRepositoryMock.Object);
+    }
+}
