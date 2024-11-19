895a5e7821cb2aa80ad7713a145ed41fe337a529
mfel3004 <martinfelder19@gmail.com>
2024-11-19 02:15:29 -0300

    [GREEN] OnException Tests for : UnauthorizedAcces, DeviceImporter, SessionException, ArgumentNullException

--- SmartHome.WebApiTest/ExceptionFilterTest.cs
+++ SmartHome.WebApiTest/ExceptionFilterTest.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Routing;
 using Moq;
+using SmartHome.BusinessLogic.CustomExceptions;
 using SmartHome.WebApi.Filters;
 
 namespace SmartHome.WebApi.Test;
@@ -51,6 +52,86 @@ public class ExceptionFilterTests
             "The status code should be 500 Internal Server Error.");
 
         Assert.AreEqual("Something went wrong. See: System.Exception Not registered", GetMessage(concreteResponse.Value));
+    }
+
+    [TestMethod]
+    public void OnException_WhenUnauthorizedAccessException_ShouldResponseUnauthorized()
+    {
+        _context.Exception = new UnauthorizedAccessException("Unauthorized access");
+
+        _attribute.OnException(_context);
+
+        var response = _context.Result;
+
+        Assert.IsNotNull(response, "The response should not be null.");
+
+        var concreteResponse = response as ObjectResult;
+        Assert.IsNotNull(concreteResponse, "The response should be of type ObjectResult.");
+
+        Assert.AreEqual((int)HttpStatusCode.Unauthorized, concreteResponse.StatusCode,
+            "The status code should be 401 Unauthorized.");
+
+        Assert.AreEqual("Unauthorized access", GetMessage(concreteResponse!.Value!));
+    }
+
+    [TestMethod]
+    public void OnException_WhenDeviceImporterException_ShouldResponseBadRequest()
+    {
+        _context.Exception = new DeviceImporterException("Device importer error");
+
+        _attribute.OnException(_context);
+
+        var response = _context.Result;
+
+        Assert.IsNotNull(response, "The response should not be null.");
+
+        var concreteResponse = response as ObjectResult;
+        Assert.IsNotNull(concreteResponse, "The response should be of type ObjectResult.");
+
+        Assert.AreEqual((int)HttpStatusCode.BadRequest, concreteResponse.StatusCode,
+            "The status code should be 400 Bad Request.");
+
+        Assert.AreEqual("Device importer error", GetMessage(concreteResponse!.Value!));
+    }
+
+    [TestMethod]
+    public void OnException_WhenSessionException_ShouldResponseBadRequest()
+    {
+        _context.Exception = new SessionException("Session error");
+
+        _attribute.OnException(_context);
+
+        var response = _context.Result;
+
+        Assert.IsNotNull(response, "The response should not be null.");
+
+        var concreteResponse = response as ObjectResult;
+        Assert.IsNotNull(concreteResponse, "The response should be of type ObjectResult.");
+
+        Assert.AreEqual((int)HttpStatusCode.BadRequest, concreteResponse.StatusCode,
+            "The status code should be 400 Bad Request.");
+
+        Assert.AreEqual("Session error", GetMessage(concreteResponse!.Value!));
+    }
+
+    [TestMethod]
+    public void OnException_WhenArgumentNullException_ShouldResponseBadRequest()
+    {
+        _context.Exception = new ArgumentNullException("Parameter name", "Argument cannot be null");
+
+        _attribute.OnException(_context);
+
+        var response = _context.Result;
+
+        Assert.IsNotNull(response, "The response should not be null.");
+
+        var concreteResponse = response as ObjectResult;
+        Assert.IsNotNull(concreteResponse, "The response should be of type ObjectResult.");
+
+        Assert.AreEqual((int)HttpStatusCode.BadRequest, concreteResponse.StatusCode,
+            "The status code should be 400 Bad Request.");
+
+        Assert.AreEqual("Argument cannot be null (Parameter 'Parameter name')", GetMessage(concreteResponse!.Value!));
     }
 
     private string GetMessage(object value)
